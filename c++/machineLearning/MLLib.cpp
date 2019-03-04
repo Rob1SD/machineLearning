d9d0d9da125d3a91198a7413b854fe306d8876cc
Chaudois <damien.chaudois@gmail.com>
2019-03-04 17:16:41 +0100

    implémentation des fake fonctions (retour en dur)

---
+++ c++/machineLearning/MLLib.cpp
@@ -0,0 +1,20 @@
+extern "C" {
+	using namespace std;
+	__declspec(dllexport) double *CreateModel(int inputdimension) {
+		return new double[inputdimension];
+	}
+	__declspec(dllexport) void RemoveLinearModel(double *model) {
+	}
+	__declspec(dllexport) int LinearFirstRegression(double *model, double *inputs, int inputsSize, int inputSize, double *outputs, int outputsSize) {
+		return 888;
+	}
+	__declspec(dllexport) int FitRosenblatt(double *modem, double *inputs, int inputsSize, double *outputs, int outputsSize) {
+		return 555;
+	}
+	__declspec(dllexport) double LinearClassification(double *model, double *input, int inputSize) {
+		return .25;
+	}
+	__declspec(dllexport) double LinearRegression(double *model, double *input, int inputSize) {
+		return .5;
+	}
+}
