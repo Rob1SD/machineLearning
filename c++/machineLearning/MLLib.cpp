78e85a50be6c29aa89ed1cec8bf92629b443fdb0
Chaudois <damien.chaudois@gmail.com>
2019-03-06 19:03:22 +0100

    LinearFirstRegression NOT OK (ne retourne pas correctement le resultat)

--- c++/machineLearning/MLLib.cpp
+++ c++/machineLearning/MLLib.cpp
@@ -1,7 +1,8 @@
-
+#include <iostream>
+#include <Eigen\Eigen>
 extern "C" {
 	using namespace std;
-
+	using namespace Eigen;
 	#include<stdlib.h>
 	#include<stdio.h>
 	#include<math.h>
@@ -30,8 +31,39 @@ extern "C" {
 	__declspec(dllexport) void RemoveLinearModel(double *model) {
 		delete[] model;
 	}
-	__declspec(dllexport) int LinearFirstRegression(double *model, double *inputs, int inputsSize, int inputSize, double *outputs, int outputsSize) {
-		return 999;
+	///X : la matrice des points avec X et Z
+	///Y : la matrice des points avec Y
+	///W : le model resultant de l'opération du pdf
+	///impossile de retourner un objet MatrixXd donc je parse la matrice en tableau de double avec colRetour et lineRetour pour préciser la taille des colonnes et ligne
+	__declspec(dllexport) int LinearFirstRegression(double *X, int XCol, int XLin, double* Y,int YLin,double *W,int* colRetour,int* LineRetour) {
+		//on init nos matrice en se basant sur les tableaux de double
+		MatrixXd MatX(XLin, XCol+1);//avec une colone en plus
+		for (int i = 0; i < XLin; i++) {
+			for (int j = 0; j < XCol; j++) {
+				MatX(i, j) = X[(i*XCol) + j];				
+			}
+			MatX(i, XCol) = 1;//on rajoute le 1 dans la dernière colone
+		}
+		MatrixXd MatY(YLin, 1);
+		for (int i = 0; i< YLin; i++) {
+			MatY(i, 0) = Y[i];
+		}
+
+		//calcule du pdf
+		MatrixXd result = ((MatX.transpose()*MatX).inverse()*MatX.transpose())*MatY;
+
+		//on renseigne le format de la matrice de resultat pour pouvoir parser le tableau de double qu'on vas renvoyer en matrice
+		*colRetour = result.cols();
+		*LineRetour = result.rows();
+
+		//on parse la matrice en tableau de double
+		W = new double[*colRetour * *LineRetour];
+		for (int i = 0; i < *LineRetour; i++) {
+			for (int j = 0; j < *colRetour; j++) {
+				W[(i * *colRetour) + j] = result(i, j);
+			}
+		}
+		return 1;
 	}
 
 	__declspec(dllexport) double LinearClassification(double *model, double *input, int inputSize) {
@@ -141,24 +173,40 @@ extern "C" {
 
 	int main(int argc, char *argv[])
 	{
-		double* model = CreateModel(3);
 
-		double* array = new double[6];
+		//double* model = CreateModel(3);
 
-		array[0] = 1;
-		array[1] = 2;
-		array[2] = 5;
-		array[3] = 3;
-		array[4] = 6;
-		array[5] = 3;
+		double* fakeY = new double[9];
 
-		int res = FitRosenblatt(model, array, 3, 2, new double[3] {1, -1, -1} , 3, 0.01, 500);
+		fakeY[0] = 1;
+		fakeY[1] = 2;
+		fakeY[2] = 5;
 
-		LinearClassification(model, new double[2]{ 3, 1 }, 2);
-		LinearClassification(model, new double[2]{ 1, 5 }, 2);
+		double *fakeX = new double[9];
+	
 
-		char* temp = new char[50];
-		scanf_s("%s", &temp);
+		fakeX[0] = 0;
+		fakeX[1] = 4;
+		fakeX[2] = 5;
+		fakeX[3] = 9;
+		fakeX[4] = 7;
+		fakeX[5] = 2;
+		fakeX[6] = 1;
+		fakeX[7] = 3;
+		fakeX[8] = 0;
+
+		double* resultat = new double[9];
+		int *ligne=new int(), *col=new int();
+ 		LinearFirstRegression(fakeX, 3, 3, fakeY, 3, resultat, col, ligne);
+
+		//int res = FitRosenblatt(model, array, 3, 2, new double[3] {1, -1, -1} , 3, 0.01, 500);
+
+		//LinearClassification(model, new double[2]{ 3, 1 }, 2);
+		//LinearClassification(model, new double[2]{ 1, 5 }, 2);
+
+		//char* temp = new char[50];
+		//scanf_s("%s", &temp);
+
 
 	}
 }
